1d265611c32d7fa22d483e47d1e4557a1a624300
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-08-08 17:25:29 +0530

    Create createBT.java

---
+++ createBT.java
@@ -0,0 +1,13 @@
+public class Main{
+        String[] arr;
+        int lastUsedIndex;
+        
+        public Main(int size){
+            arr = new String[size+1];
+            this.lastUsedIndex=0;
+            System.out.println("Blank tree of" +size+"!");
+        }
+        public static void main(String[] args){
+        Main ritesh = new Main(5);
+    }
+}
